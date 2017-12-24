94bbb9800cb49a80edc9aaa45efe88c19f04da4f
Stuart George <stuart.bigmassa@gmail.com>
2017-12-24 00:53:15 +0000

    added latest lists on home page

--- app/templatetags/app_tags.py
+++ app/templatetags/app_tags.py
@@ -1,8 +1,18 @@
 from django.template import Library
 
+from docs.models import Page, Snippet
+
 register = Library()
 
 
 @register.filter(name='addcss')
 def addcss(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+@register.inclusion_tag(filename='tags/latest.html')
+def latest_pages(count):
+    return {'objects': Page.objects.all().order_by('-updated_on')[:count]}
+
+@register.inclusion_tag(filename='tags/latest.html')
+def latest_snippets(count):
+    return {'objects': Snippet.objects.all().order_by('-updated_on')[:count]}
