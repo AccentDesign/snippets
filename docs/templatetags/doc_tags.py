165b93bf32aa76003f622c58f978025b0899c4bd
Stuart George <stuart.bigmassa@gmail.com>
2017-12-28 16:13:50 +0000

    added latest snippet to homepage

--- docs/templatetags/doc_tags.py
+++ docs/templatetags/doc_tags.py
@@ -13,3 +13,11 @@ def latest_pages(count):
 @register.inclusion_tag(filename='docs/tags/latest.html')
 def latest_snippets(count):
     return {'objects': Snippet.objects.all().order_by('-updated_on')[:count]}
+
+@register.inclusion_tag(filename='docs/tags/latest_snippet.html')
+def latest_snippet():
+    try:
+        snippet = Snippet.objects.latest('created_on')
+    except Snippet.DoesNotExist:
+        snippet = None
+    return {'snippet': snippet}
