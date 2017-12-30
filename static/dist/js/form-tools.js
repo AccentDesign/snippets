53baffb18a12eeba6738bdbb4af6594ca9648d9b
Stuart George <stuart.bigmassa@gmail.com>
2017-12-30 00:00:05 +0000

    page content items sortable

--- static/dist/js/form-tools.js
+++ static/dist/js/form-tools.js
@@ -1,46 +1,119 @@
-function cloneFormsetRow(selector, type) {
-    var $newElement = $(selector).clone(true, true).removeClass('empty-form');
-    var $total = $('#id_' + type + '-TOTAL_FORMS').val();
+$.fn.formset = function () {
+    /**
+     * Use to dynamically create and reorder new formsets
+     */
+    var _this = this;
+    var formClass = $(this).data('form-class');
+    var emptyFormClass = $(this).data('empty-form-class');
+    var prefix = $(this).data('form-prefix');
+    var firstFormClass = 'first';
+    var lastFormClass = 'last';
+    var deletedFormClass = 'deleted';
 
-    $newElement.find(':input, select, a, span, label').each(function() {
-        if ($(this).attr('id')) {
-            var $id = $(this).attr('id').replace('__prefix__', $total);
-            $(this).attr({'id': $id});
+    function makeClone() {
+        var $original = $('.' + emptyFormClass);
+        var $new = $($original).clone(true, true);
+        $($new).removeClass(emptyFormClass);
+        $($new).addClass(formClass);
+
+        var $total = $('#id_' + prefix + '-TOTAL_FORMS').val();
+
+        $new.find(':input, select, a, span, label').each(function() {
+            if ($(this).attr('id')) {
+                var $id = $(this).attr('id').replace('__prefix__', $total);
+                $(this).attr({'id': $id});
+            }
+            if ($(this).attr('name')) {
+                var $name = $(this).attr('name').replace('__prefix__', $total);
+                $(this).attr({'name': $name});
+            }
+            if ($(this).attr('for')) {
+                var $for = $(this).attr('for').replace('__prefix__', $total);
+                $(this).attr({'for': $for});
+            }
+        });
+
+        $total++;
+
+        $('#id_' + prefix + '-TOTAL_FORMS').val($total);
+
+        // if there is a markdownx re initialize it
+        markdownx = $($new).find('.markdownx');
+        if (markdownx) {
+            new window.MarkdownX(
+                markdownx[0],
+                markdownx[0].querySelector('.markdownx-editor'),
+                markdownx[0].querySelector('.markdownx-preview')
+            );
         }
-        if ($(this).attr('name')) {
-            var $name = $(this).attr('name').replace('__prefix__', $total);
-            $(this).attr({'name': $name});
-        }
-        if ($(this).attr('for')) {
-            var $for = $(this).attr('for').replace('__prefix__', $total);
-            $(this).attr({'for': $for});
+
+        $($original).before($new);
+    }
+
+    function visibleForms() {
+        return $(_this).find('.' + formClass + ':visible');
+    }
+
+    function updateOrder() {
+        visibleForms().each(function(index) {
+            $( this ).find('input[id*="-order"]').val(index);
+        });
+    }
+
+    function updateActions() {
+        visibleForms().each(function(index) {
+            $(this).removeClass(firstFormClass).removeClass(lastFormClass);
+            if (index == 0) {
+                $(this).addClass(firstFormClass);
+            } else if (index == visibleForms().length - 1) {
+                $(this).addClass(lastFormClass);
+            }
+        });
+    }
+
+    // make sortable
+
+    $(_this).sortable({
+        forcePlaceholderSize: true,
+        cursor: "move",
+        update: function(event, ui) {
+            updateOrder();
+            updateActions();
         }
     });
 
-    $total++;
+    // actions
 
-    $('#id_' + type + '-TOTAL_FORMS').val($total);
+    $(_this).on('click', '.action-up', function(){
+        var current = $(this).closest('.' + formClass);
+        current.prev().before(current);
+        updateOrder();
+        updateActions();
+    });
 
-    // if there is a markdownx re initialize it
-    markdownx = $($newElement).find('.markdownx');
-    if (markdownx) {
-        new window.MarkdownX(
-            markdownx[0],
-            markdownx[0].querySelector('.markdownx-editor'),
-            markdownx[0].querySelector('.markdownx-preview')
-        );
-    }
+    $(_this).on('click', '.action-down', function(){
+        var current = $(this).closest('.' + formClass);
+        current.next().after(current);
+        updateOrder();
+        updateActions();
+    });
 
-    $(selector).before($newElement);
-}
+    $(_this).on('click', '.add-row', function(){
+        makeClone();
+        updateOrder();
+        updateActions();
+    });
 
-$(document).on('change', '.formset input[id*="-DELETE"]', function () {
-    if ($(this).is(':checked')) {
-        $(this).closest('.formset').addClass('deleted');
-    } else {
-        $(this).closest('.formset').removeClass('deleted');
-    }
-});
+    $(_this).on('change', 'input[id*="-DELETE"]', function () {
+        if ($(this).is(':checked')) {
+            $(this).closest('.' + formClass).addClass(deletedFormClass);
+        } else {
+            $(this).closest('.' + formClass).removeClass(deletedFormClass);
+        }
+    });
+
+    return _this;
+};
 
 function slugify(text) {
     return text.toString().toLowerCase()
