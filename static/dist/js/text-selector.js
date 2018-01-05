43bb78f446620eb8645ef74cfbc4abb1427d5d9e
Stuart George <stuart.bigmassa@gmail.com>
2018-01-05 22:53:07 +0000

    select code inside highlight when clicked

---
+++ static/dist/js/text-selector.js
@@ -0,0 +1,21 @@
+function SelectText(element) {
+    var doc = document
+        , range
+        , selection
+    ;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+$(document).on('click', '.highlight', function () {
+    SelectText(this);
+});
